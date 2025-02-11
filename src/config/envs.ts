5b363bbe341b7279d9fc04d34d7393de151daf4e
jFroylan-Cal <j.froylan_calderon@outlook.com>
2025-02-11 14:31:54 -0600

    JWT seed  implementation

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -9,6 +9,7 @@ export const envs = {
   PORT: get('PORT').required().asPortNumber(),
   MONGO_URL: get('MONGO_URL').required().asUrlString(),
   MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
+  JWT_SEED: get('JWT_SEED').required().asString(),
 
 }
 
--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -1,6 +1,9 @@
 
 import jwt, { SignOptions} from "jsonwebtoken";
+import { envs } from "./envs";
 
+
+const JWT_SEED = envs.JWT_SEED;
 
     export class JwtAdapter {
         
@@ -12,7 +15,7 @@ import jwt, { SignOptions} from "jsonwebtoken";
          */
         static async generateToken(payload: any, duration: string = '2h') {
             return new Promise((resolve) => { 
-                jwt.sign(payload, "seed", { expiresIn: duration } as SignOptions, (err, token) => {
+                jwt.sign(payload, JWT_SEED, { expiresIn: duration } as SignOptions, (err, token) => {
                     
                     if (err) return resolve(null);
 
